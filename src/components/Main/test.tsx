7e2ce1d3c1342793d0ddaefed4836bd15d40d9b4
Pedro Dalvy <pedrodalvy@outlook.com>
2021-12-25 10:26:24 -0400

    add styles on example component

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -6,9 +6,15 @@ describe('<Main />', () => {
     const { container } = render(<Main />);
 
     expect(
-      screen.getByRole('heading', { name: /react avançado/i })
+      screen.getByRole('heading', { name: /boilerplate nextjs/i })
     ).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
   });
 });
