086ecd8d60b042ea0c7be6ece3ddb640fe8693dd
Pedro Dalvy <pedrodalvy@outlook.com>
2021-12-25 15:20:19 -0400

    change storybook addon knobs to essentials

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,7 @@ module.exports = {
   ],
   'addons': [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs'
+    '@storybook/addon-essentials'
   ],
   'framework': '@storybook/react',
 };
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,11 +1,17 @@
 import Main from './index';
-import { ComponentMeta } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { Meta, Story } from '@storybook/react';
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
-} as ComponentMeta<typeof Main>;
+  args: {
+    title: 'Boilerplate Storybook',
+  },
+} as Meta;
 
-export const Basic = () => <Main title={text('title', 'Boilerplate NextJS')} />;
+export const Basic: Story = (args) => <Main {...args} />;
+
+export const Default: Story = (args) => <Main {...args} />;
+Default.args = {
+  title: 'Boilerplate NextJS',
+};
